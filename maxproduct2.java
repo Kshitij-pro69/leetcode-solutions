cca826205818a1507af6146d6ef1f8942cb4b90b
Kshitij-pro69 <kshitijkadyan3@gmail.com>
2026-07-26 23:03:05 +0530

    solved: maxproduct2

---
+++ maxproduct2.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maximumProduct(int[] nums) {
+        // Initialize the three largest numbers and two smallest numbers
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        
+        for (int num : nums) {
+            // Update three largest
+            if (num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (num > max3) {
+                max3 = num;
+            }
+            
+            // Update two smallest
+            if (num < min1) {
+                min2 = min1;
+                min1 = num;
+            } else if (num < min2) {
+                min2 = num;
+            }
+        }
+        
+        // Two possible maximum products:
+        // 1. Product of three largest numbers
+        // 2. Product of two smallest (most negative) numbers and the largest number
+        return Math.max(max1 * max2 * max3, min1 * min2 * max1);
+    }
+}
